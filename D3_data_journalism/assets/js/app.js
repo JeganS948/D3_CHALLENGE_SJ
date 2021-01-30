4ac1693e57ea6c80bc49f2818f77b1547bff4d30
JeganS948 <69284232+JeganS948@users.noreply.github.com>
2021-01-30 11:03:58 -0500

    Update app.js

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,1 +1,26 @@
-// @TODO: YOUR CODE HERE!
+// D3 Homework challenge - Data Journalism 
+
+// Chart setup
+// Automatic resize of the chart
+function makeResponsive() {
+
+    // Loads, remove & replace browser with a resized version of chart [condition: SVG Area is not empty]
+    var svgArea = d3.select("body").select("svg");
+
+    // Clear SVG [if condition not met]
+    if(!svgArea.empty()) {
+        svgArea.remove();
+    }
+
+    // SVG parameter setup
+    var svgWidth = 980;
+    var svgHeight = 600;
+
+    // SVG margin setup
+    var margin = {
+        top: 20,
+        right: 40,
+        bottom: 90,
+        left: 100,
+    };
+}
